2d0f9622018e1d1572d13d78a9ce7b53fefec875
amargh2 <margherioa@gmail.com>
2022-08-15 16:49:03 -0500

    got it working! trust the recursion. haha.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const fibsRec4 = (n, fibArray=[0, 1]) => n > 0 ? fibsRec4(n -1, fibArray.concat((fibArray.at(-1) + fibArray.at(-2)))) : fibArray 
 
-const dummyArray = [3, 5, 44, 55, 99, 8, 4, 1, 2, 6, 7, 9, 10, 15000, 1000000, 2500, 88, 66, 44, 96]
+const dummyArray = [105,77, 102, 85, 2000, 66, 1, 103, 22, 488, 9222, 868, 109, 110, 757, 44, 32, 2, 88,
+  56, 106, 450000000000, 101, 585984156, 107, 54890262, 689458, 104, 88888888]
 
 const sortMerge3 = (array, sorted=[]) => {
 
@@ -16,6 +17,8 @@ const sortMerge3 = (array, sorted=[]) => {
 
   let left = []
   let right = []
+  let rightSort = []
+  let leftSort = []
 
   left = array.slice(0, getDivideFactor(array))
   right = array.slice(getDivideFactor(array))
@@ -70,9 +73,9 @@ const sortMerge3 = (array, sorted=[]) => {
     console.log('its this one')
     let sortedRight = []
     let sortedLeft = []
-    sortMerge3(right, sortedRight)
-    sortMerge3(left, sortedLeft)
-    merge(sortedRight, sortedLeft, sorted)
+    right.length >=3 ? sortMerge4(right, sortedRight) : sort(right, sortedRight)
+    left.length>=3 ? sortMerge4(left, sortedLeft) : sort(left, sortedLeft)
+    merge(sortedRight)
   }
 
 
@@ -92,7 +95,9 @@ const sortMerge4 = (array, sorted=[]) => {
 
   let left = []
   let right = []
-
+  let sortLeft = []
+  let sortRight = []
+  
   left = array.slice(0, getDivideFactor(array))
   right = array.slice(getDivideFactor(array))
   console.log('left = ' + left + ' right= ' + right)
@@ -111,6 +116,7 @@ const sortMerge4 = (array, sorted=[]) => {
 
   function merge(rightSorted, leftSorted, suppliedArray =[]) {
     const number = rightSorted.length + leftSorted.length
+    console.log('heres me sending the thing ' + rightSorted, leftSorted)
     for (let i = 0; i < number; i++) {
       if (leftSorted.length === 0 && rightSorted.length !== 0) {
         suppliedArray.push(rightSorted[0])
@@ -128,33 +134,40 @@ const sortMerge4 = (array, sorted=[]) => {
         suppliedArray.push(leftSorted[0])
         leftSorted.splice(0,1)
       } 
-    }    
-    console.log('supplied array = ' + suppliedArray)
+    }
     return suppliedArray
   }
+    
+    if (left.length>= 3 && right.length>= 3) {
+      let sortedRight = []
+      let sortedLeft = []
+      sortMerge4(right, sortRight)
+      sortMerge4(left, sortLeft)
+      merge(sortRight, sortLeft, sorted)
+    }
 
-  if (left.length <= 2 && right.length <= 2){
-    let sortedLeft = sort(left)
-    let sortedRight = sort(right)
-    togetherNow = []
-    console.log(sortedLeft, sortedRight)
-    merge(sortedRight, sortedLeft, sorted)
-    console.log(togetherNow)
+    if (left.length>= 3 && right.length <= 2) {
+      sortMerge4(left, sortLeft)
+      sortRight = sort(right)
+      merge(sortRight, sortLeft, sorted)
+    }
+
+    if (left.length <= 2 && right.length <= 2){
+      let sortLeft = sort(left)
+      let sortRight = sort(right)
+      togetherNow = []
+      console.log('herp ' + merge(sortRight, sortLeft, sorted))
     } 
-
-  if (right.length >= 3 || left.length>=3) {
-    console.log('its this one')
-    let sortedRight = []
-    let sortedLeft = []
-    sortMerge3(right, sortedRight)
-    sortMerge3(left, sortedLeft)
-    merge(sortedRight, sortedLeft, sorted)
-  }
-
-
-  console.log('final =' + sorted, 'initial = ' + array)
   
+
+  merge(sortRight, sortLeft, sorted)
+  console.log('grow up ' + sortRight, 'blow away ' + sortLeft)
+  console.log('final =' + sorted, 'initial = ' + array)
+  console.log(sorted)
+  return sorted
 }
 
 
-sortMerge3(dummyArray)
+sortMerge4(dummyArray)
+
+sortMerge4([22, 55, 1])
